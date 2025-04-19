8cd48c3feb86cf2b0dcce46cc3670c6c2742b53f
Timon <yurikikadet@gmail.com>
2025-04-19 23:26:00 +0600

    написан класс для работы с двоичным потоком

---
+++ src/main/java/classes/BinaryAndCharacterStream.java
@@ -0,0 +1,38 @@
+package classes;
+
+import java.io.*;
+
+public class BinaryAndCharacterStream {
+
+    public static void writeArrayToBinaryFile(int[] array, String fileName) throws IOException {
+        try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(fileName))) {
+            dataOutputStream.writeInt(array.length);
+            for (int num : array) {
+                dataOutputStream.writeInt(num);
+            }
+        }
+    }
+
+    public static int[] readArrayFromBinaryFile(String fileName) throws IOException {
+        try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream(fileName))) {
+            int length = dataInputStream.readInt();
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++) {
+                array[i] = dataInputStream.readInt();
+            }
+            return array;
+        }
+    }
+
+    public static void writeArrayToCharacterFile(int[] array, String fileName) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (int i = 0; i < array.length; i++) {
+                writer.write(String.valueOf(array[i]));
+                if (i < array.length - 1) {
+                    writer.write(" ");
+                }
+            }
+        }
+    }
+
+}
